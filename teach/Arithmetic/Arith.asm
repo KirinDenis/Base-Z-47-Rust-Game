7c44c27a98cbe2f3525d620bcfafd3039e79255f
Denis Kirin <39277935+KirinDenis@users.noreply.github.com>
2024-09-09 19:29:47 +0200

    Teach 🧮 ADC Instruction

--- teach/Arithmetic/Arith.asm
+++ teach/Arithmetic/Arith.asm
@@ -47,5 +47,36 @@
         mov ax,00FFh
         aam         ; ERROR Parity Flag is UP
 
+;ADC (Add with Carry)
+;https://github.com/KirinDenis/Mystery-of-Base-Z-47-DOS-Game/wiki/%F0%9F%A7%AE-ADC-Instruction
+
+	mov al, 01h    ; Load 1 into AL
+	mov bl, 01h    ; Load 1 into BL
+	adc al, bl     ; AL = AL + BL + No Carry flag
+
+	mov al, 0FFh   ; Load FF into AL
+	mov bl, 001h   ; Load 1 into BL
+	adc al, bl     ; AL = AL + BL + Carry flag = 00h -> In the 8086, byte register addition is cyclic—adding 1 to 0xFF results in 0x00 and sets the overflow flag.
+	               ; Flags:
+                       ; - Carry flag UP -> for this instruction is mean result is 100h = FFh + 1h
+                       ; - Zero flag UP -> formaly the result is 0
+                       ; - Parity Flag is UP -> 0 is even number
+                       ; - Auxiliary Carry Flag is UP ->  a carry is a digit that is transferred from one column of digits to another column of more significant digits?
+
+	mov al, 0FFh   ; Load FF into AL
+	mov bl, 002h   ; Load 1 into BL
+	adc al, bl     ; AL = AL + BL + Carry flag = 01h (101h) -> byte register cyclic—adding
+                       ; Flags: Carry flag UP and Auxiliary Carry flag is UP
+
+	mov al, 0FFh   ; Load FF into AL
+	mov bl, 0FFh   ; Load 1 into BL      
+	adc al, bl     ; AL = AL + BL + Carry flag = FEh -> FFh + FFh = 1FEh (-2 decimal -> SEE: Sign Flag flag)
+                       ; Flags: 
+                       ; - Carry flag UP
+                       ; - Auxiliary Carry flag is UP
+                       ; - Sign Flag is UP the value is negative
+
+
+
 	ret
 end
