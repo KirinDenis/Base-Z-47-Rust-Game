073de0386bf2cacf33aeea2076a6ca625f2e9860
Denis Kirin <39277935+KirinDenis@users.noreply.github.com>
2025-04-17 19:11:40 +0200

    Draw image to canvas

--- WASM/base-z47/src/lib.rs
+++ WASM/base-z47/src/lib.rs
@@ -2,44 +2,23 @@ mod utils;
 
 mod view;
 
-
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{window, Document, HtmlElement, KeyboardEvent, HtmlCanvasElement, CanvasRenderingContext2d};
+use web_sys::{
+    window, CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlElement, KeyboardEvent,
+};
 
 use std::thread;
 use std::time::Duration;
 
-fn draw_squares(ctx: &CanvasRenderingContext2d, width: u32, height: u32) {
-    use js_sys::Math::random;
-    
-    let square_size = 20;
-    let cols = width / square_size;
-    let rows = height / square_size;
-    
-    for x in 0..cols {
-        for y in 0..rows {
-            let r = (random() * 255.0) as u8;
-            let g = (random() * 255.0) as u8;
-            let b = (random() * 255.0) as u8;
-            let color = format!("rgb({}, {}, {})", r, g, b);
-            ctx.set_fill_style(&color.into());
-            ctx.fill_rect(
-                (x * square_size) as f64,
-                (y * square_size) as f64,
-                square_size as f64,
-                square_size as f64,
-            );
-        }
-    }
-}
-
-fn init()-> Result<(), JsValue> {
+fn init() -> Result<(), JsValue> {
     let document = window().unwrap().document().unwrap();
-    let canvas = document.create_element("canvas")?.dyn_into::<HtmlCanvasElement>()?;
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<HtmlCanvasElement>()?;
     let body = document.body().unwrap();
     body.append_child(&canvas)?;
 
@@ -51,6 +30,7 @@ fn init()-> Result<(), JsValue> {
     let context = Rc::new(RefCell::new(context));
     let canvas = Rc::new(canvas);
 
+    /*
     // Resize canvas to full window size
     let context_clone = Rc::clone(&context);
     let canvas_clone = Rc::clone(&canvas);
@@ -59,116 +39,89 @@ fn init()-> Result<(), JsValue> {
         let height = window().unwrap().inner_height().unwrap().as_f64().unwrap() as u32;
         canvas_clone.set_width(width);
         canvas_clone.set_height(height);
-        draw_squares(&context_clone.borrow(), width, height);
+        view::draw_image_ex(&context_clone.borrow(), width, height, 0, 0);
     }) as Box<dyn FnMut()>);
 
-    window().unwrap().set_onresize(Some(closure.as_ref().unchecked_ref()));
-    closure.forget(); 
 
-    // Initial drawing
-    let width = canvas.width();
-    
-    let height = canvas.height();
+    window()
+        .unwrap()
+        .set_onresize(Some(closure.as_ref().unchecked_ref()));
+    closure.forget();
+     */
 
-    
+    let width = window().unwrap().inner_width().unwrap().as_f64().unwrap() as u32;
+    let height = window().unwrap().inner_height().unwrap().as_f64().unwrap() as u32;
+
+    canvas.set_width(width);
+    canvas.set_height(height);
+
     let ctx = context.borrow();
-     draw_squares(&context.borrow(), width, height);    
-     Ok(())
-}
 
+    view::draw_image_ex(&context.borrow(), width, height, 0, 0);
+    Ok(())
+}
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-
     init();
 
-    Ok(())
+
+
+   //     view::draw_image_ex(0,0);
+
+        Ok(())
+
 /*
-    let console = document.create_element("div").unwrap();
-    console.set_attribute("id", "console").unwrap();
-    console.set_attribute("tabindex", "0").unwrap(); 
-    document.body().unwrap().append_child(&console).unwrap();
+        let mut imagecount = 0;
+        let closure = Closure::wrap(Box::new(move |event: KeyboardEvent| {
+            let key = event.key();
 
-    let pre = document.create_element("pre").unwrap();
-    let code = document.create_element("code").unwrap();
-    code.set_attribute("id", "code").unwrap();
-    pre.append_child(&code); 
+            let document = window().unwrap().document().unwrap();
+            let console = document.get_element_by_id("console").unwrap();
+            let last_line = console.last_element_child().unwrap();
+            let content = last_line.first_element_child().unwrap();
+            let cursor = last_line.last_element_child().unwrap();
 
-    let line = document.create_element("div").unwrap();
-    line.set_attribute("class", "line").unwrap();
-    code.append_child(&line); 
+            if key == "Enter" {
 
-    let content = document.create_element("span").unwrap();
-    content.set_attribute("class", "content").unwrap();
-    line.append_child(&content).unwrap();
+                cursor.set_text_content(None);
+                let new_line = document.create_element("div").unwrap();
+                new_line.set_attribute("class", "line").unwrap();
 
+                let new_content = document.create_element("span").unwrap();
+                new_content.set_attribute("class", "content").unwrap();
+                new_line.append_child(&new_content).unwrap();
 
-    let mut text = content.text_content().unwrap_or_default();
-    text.push_str("Base-Z47");
-    content.set_text_content(Some(&text));
+                let new_cursor = document.create_element("span").unwrap();
+                new_cursor.set_attribute("class", "cursor").unwrap();
+                new_cursor.set_text_content(Some("_"));
+                new_line.append_child(&new_cursor).unwrap();
 
+                console.append_child(&new_line).unwrap();
 
-    let cursor = document.create_element("span").unwrap();
-    cursor.set_attribute("class", "cursor").unwrap();
-    cursor.set_text_content(Some("_"));
-    line.append_child(&cursor).unwrap();
+                imagecount = imagecount + 1;
+                view::draw_image_ex(imagecount,0);
+            } else if key == "Backspace" {
 
-    console.append_child(&pre).unwrap();
+                let mut text = content.text_content().unwrap_or_default();
+                text.pop();
+                content.set_text_content(Some(&text));
+            } else if key.len() == 1 {
 
-    let colors = ["red", "blue", "green", "purple", "brown", "black"];
-    let mut text = code.text_content().unwrap_or_default();
+                let mut text = content.text_content().unwrap_or_default();
+                text.push_str(&key);
+                content.set_text_content(Some(&text));
 
-    view::draw_image_ex(0,0);
-
-    let mut imagecount = 0; 
-    let closure = Closure::wrap(Box::new(move |event: KeyboardEvent| {
-        let key = event.key();
-
-        let document = window().unwrap().document().unwrap();
-        let console = document.get_element_by_id("console").unwrap();
-        let last_line = console.last_element_child().unwrap();
-        let content = last_line.first_element_child().unwrap(); 
-        let cursor = last_line.last_element_child().unwrap(); 
-
-        if key == "Enter" {
-
-            cursor.set_text_content(None); 
-            let new_line = document.create_element("div").unwrap();
-            new_line.set_attribute("class", "line").unwrap();
-
-            let new_content = document.create_element("span").unwrap();
-            new_content.set_attribute("class", "content").unwrap();
-            new_line.append_child(&new_content).unwrap();
-
-            let new_cursor = document.create_element("span").unwrap();
-            new_cursor.set_attribute("class", "cursor").unwrap();
-            new_cursor.set_text_content(Some("_"));
-            new_line.append_child(&new_cursor).unwrap();
-
-            console.append_child(&new_line).unwrap();
-
-            imagecount = imagecount + 1;
-            view::draw_image_ex(imagecount,0); 
-        } else if key == "Backspace" {
-
-            let mut text = content.text_content().unwrap_or_default();
-            text.pop(); 
-            content.set_text_content(Some(&text));
-        } else if key.len() == 1 {
-
-            let mut text = content.text_content().unwrap_or_default();
-            text.push_str(&key);
-            content.set_text_content(Some(&text));
-           
-        }
+            }
 
 
-        console.scroll_top();
-    }) as Box<dyn FnMut(_)>);
+            console.scroll_top();
+        }) as Box<dyn FnMut(_)>);
 
-    console.add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref()).unwrap();
-    closure.forget();
-*/
+        console.add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+ */       
+
 }
 fn log_to_console(message: &str) {
     let document = window().unwrap().document().unwrap();
@@ -187,7 +140,7 @@ fn append_to_console(input: &str) {
 }
 
 #[wasm_bindgen]
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 extern "C" {
     fn alert(s: &str);
 }
--- WASM/base-z47/src/view.rs
+++ WASM/base-z47/src/view.rs
@@ -1,38 +1,63 @@
 pub mod image0;
-//pub mod image1;
-//pub mod image2;
-//pub mod image3;
-//pub mod image4;
+pub mod image1;
+pub mod image2;
+pub mod image3;
+pub mod image4;
 
 //use rgb::RGB8;
 use hex::encode;
 
 use wasm_bindgen::prelude::*;
-use web_sys::{window, Document, HtmlElement, KeyboardEvent};
+use web_sys::{window, Document, HtmlElement, KeyboardEvent, HtmlCanvasElement, CanvasRenderingContext2d};
 
 
 
-pub fn intro_image(image: usize) {
+pub fn draw_squares(ctx: &CanvasRenderingContext2d, width: u32, height: u32) {
+    use js_sys::Math::random;
+    
+    let square_size = 20;
+    let cols = width / square_size;
+    let rows = height / square_size;
+    
+    for x in 0..cols {
+        for y in 0..rows {
+            let r = (random() * 255.0) as u8;
+            let g = (random() * 255.0) as u8;
+            let b = (random() * 255.0) as u8;
+            let color = format!("rgb({}, {}, {})", r, g, b);
+            ctx.set_fill_style(&color.into());
+            ctx.fill_rect(
+                (x * square_size) as f64,
+                (y * square_size) as f64,
+                square_size as f64,
+                square_size as f64,
+            );
+        }
+    }
+}
+
+
+pub fn intro_image(ctx: &CanvasRenderingContext2d, width: u32, height: u32,image: usize) {
     for i in (0..8).rev() {
-        draw_image_ex(image, i);
+        draw_image_ex(ctx, width, height, image, i);
        // thread::sleep(Duration::from_millis(100));
     }
 }
 
-pub fn draw_image(image: usize) {
-    draw_image_ex(image, 0);
+pub fn draw_image(ctx: &CanvasRenderingContext2d, width: u32, height: u32,image: usize) {
+    draw_image_ex(ctx, width, height, image, 0);
 }
 
+
 #[wasm_bindgen]
-pub fn draw_image_ex(image: usize, hide: u8) {
+pub fn draw_image_ex(ctx: &CanvasRenderingContext2d, width: u32, height: u32, image: usize, hide: u8) {
 
-    let mut buffer: Vec<String> = Vec::new();
-
-//    let (_sh, _sw) = term.size();
+  
     
     let _sh = 60;
     let _sw = 200;
     
+    let square_size = width / _sw;
 
     let pixels;
 
@@ -40,91 +65,69 @@ pub fn draw_image_ex(image: usize, hide: u8) {
         pixels = image0::get();
     } 
       else if image == 1 {
-  pixels = image0::get();
+  pixels = image1::get();
     } else if image == 2 {
-  pixels = image0::get();
+  pixels = image2::get();
     } else if image == 3 {
-  pixels = image0::get();
+  pixels = image3::get();
     } else {
-  pixels = image0::get();
+  pixels = image4::get();
     }
-
-  //  term.move_cursor_to(0, 0).unwrap();
-
-    let psize = 56000;
+  
+    let psize = 74000;
     let lsize = 200 * 3;
-//    let psize = 560;
-//    let lsize = 20 * 3;
 
     let mut count: usize = 0;
 
+    let mut x = 0;
+    let mut y = 0;
+
     for i in (0..psize).step_by(lsize * 2) {
-        let mut s1: String = Default::default();
+        
 
         for j in (i..i + lsize).step_by(3) {
 
-           let fc = encode(&pixels[j + 0..j + 3]);
+         //  let fc = encode(&pixels[j + 0..j + 3]);
          //      format!("#{:02X}", pixels[j + 0]);
         //        format!("{:02X}", pixels[j + 1]) +
         //        format!("{:02X}", pixels[j + 2]);
 
-           let bg = encode(&pixels[j + 0 + lsize..j + 3 + lsize]);
+        let r = (pixels[j + 0]) as u8;
+        let g = (pixels[j + 1]) as u8;
+        let b = (pixels[j + 2]) as u8;
+        let color = format!("rgb({}, {}, {})", r, g, b);
+        ctx.set_fill_style(&color.into());
+        ctx.fill_rect(
+            (x * square_size) as f64,
+            (y * square_size) as f64,
+            square_size as f64,
+            square_size as f64,
+        );
+
+        let r = (pixels[j + 0 + lsize]) as u8;
+        let g = (pixels[j + 1 + lsize]) as u8;
+        let b = (pixels[j + 2 + lsize]) as u8;
+        let color = format!("rgb({}, {}, {})", r, g, b);
+        ctx.set_fill_style(&color.into());
+        ctx.fill_rect(
+            (x * square_size) as f64,
+            ((y + 1) * square_size) as f64,
+            square_size as f64,
+            square_size as f64,
+        );
+
+
+           //let bg = encode(&pixels[j + 0 + lsize..j + 3 + lsize]);
             //   format!("#{:02X}", pixels[j + 0 + lsize]) +
             //    format!("{:02X}", pixels[j + 1 + lsize]) +
             //    format!("{:02X}", pixels[j + 2 + lsize]);
 
-              
-/*
 
-use hex::encode;
-
-fn main() {
-    let data: Vec<u8> = (0..=255).collect();
-    let hex_string = encode(&data[4..7]); 
-    println!("{}", hex_string); // "040506"
-}
-
-            let fc: RGB8 = RGB8 {
-                r: pixels[j + 0] >> hide,
-                g: pixels[j + 1] >> hide,
-                b: pixels[j + 2] >> hide,
-            };
-
-fn main() {
-    let byte: u8 = 255;
-    let hex_string = format!("{:02X}", byte); // "FF"
-    println!("{}", hex_string);
-}
-
-            let bc: RGB8 = RGB8 {
-                r: pixels[j + 0 + lsize] >> hide,
-                g: pixels[j + 1 + lsize] >> hide,
-                b: pixels[j + 2 + lsize] >> hide,
-            };
-*/
-
-//            let style: Style = Style::new()
-//                .fg(Color::Color256(ansi256_from_rgb(fc)))
-//                .bg(Color::Color256(ansi256_from_rgb(bc)));
-
-            //s1.push_str(&style.apply_to("\u{2580}").to_string());
-            s1.push_str(&format!("<span style='color: #{}; background-color: #{};'>{}</span>", fc, bg, "\u{2580}"));
+            x = x + 1;
             count = count + 1;
         }
-        count = count + 2;
-        buffer.push(s1);
-        buffer.push("<br>".to_string());
+        x = 0;
+        y =y + 2;
     }
-
-    let document = window().unwrap().document().unwrap();
-    let code = document.get_element_by_id("code");
-    code.expect("REASON").set_inner_html(&buffer.join(" "));
-       
-
-//    term.move_cursor_to(0, 0).unwrap();
-//    for text in buffer {
-//        term.write_line(&text).unwrap();
-//    }
-
 
 }
